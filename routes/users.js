6abb90d503b2c45c62a518d02a7f689cbf9ef41b
MIlind Singh <milindsingh84@gmail.com>
2015-09-04 18:42:39 -0700

    form submit,layout added

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+
+  res.render('users', { title: 'Express' });
+
+
 });
+
+router.post('/', function(req, res, next) {
+
+//var userName =  req.query['username'];
+ console.log(req.body);
+var email =  req.body.email;
+//var password =  req.body.password;
+
+console.log(email);
+
+  res.render('user_created', { title: 'Express' });
+
+
+});
+
+
+
 
 module.exports = router;
